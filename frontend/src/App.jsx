6ae7c81ad229c1bbe554458f0239684bf8426f1e
ankita_yadav87 <ankuy086@gmail.com>
2024-01-28 20:17:36 +0530

    added authentication

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,15 @@
-import { useState } from 'react'
-import Header from './components/Header/Header'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React, { useEffect } from 'react';
+import { Routes, Route, BrowserRouter as Router } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import Header from './components/Header/Header';
 import Home from './components/Home';
 import Login from './components/login/Login';
 import SignUp from './components/signup/SignUp';
 import Employee from './components/Employee';
 import Department from './components/Department';
 
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
@@ -16,7 +17,7 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Login />} />
-          <Route path="/home" element={<Login />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/employees" element={<Employee />} />
@@ -24,7 +25,7 @@ function App() {
         </Routes>
       </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
 import './header.css';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 const Header = () => {
+    const isAuthenticated = useSelector((state) => state.login.isAuthenticated);
+    console.log("header",isAuthenticated)
+
+    const handleEmployeeLinkClick = (e) => {
+        if (!isAuthenticated) {
+          alert('Please login to access this resource.');
+        }
+      };
+
     return (
         <>
             <div className='nav'>
@@ -10,8 +20,8 @@ const Header = () => {
                     <Link to="/" className="active">Home</Link>
                     <Link to="/login" >Login</Link>
                     <Link to="/signup">SignUp</Link>
-                    <Link to="/employees" >Employee</Link>
-                    <Link to="/department">Department</Link>
+                    <Link to={!isAuthenticated ? "/login" : "/employees"} onClick={(e)=>handleEmployeeLinkClick(e)}>Employee</Link>
+                    <Link to={!isAuthenticated ? "/login" : "/department"} onClick={(e)=>handleEmployeeLinkClick(e)}>Department</Link>
                 </div>
             </div>
 
